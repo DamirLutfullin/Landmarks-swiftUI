21a604adc28f42bd10f5de164e81a67647604620
Damir Lutfullin <dmr.lutfullin@gmail.com>
2020-03-08 05:27:53 +0500

    пока закончу тут

--- Landmarks/Home.swift
+++ Landmarks/Home.swift
@@ -9,8 +9,6 @@
 import SwiftUI
 
 struct CategoryHome: View {
-    @EnvironmentObject var userData : UserData
-    
     var categories: [String: [Landmark]] {
         Dictionary(
             grouping: landmarkData,
@@ -18,20 +16,56 @@ struct CategoryHome: View {
         )
     }
     
+    var featured: [Landmark] {
+        landmarkData.filter { $0.isFeatured }
+    }
     
+    @State var showingProfile = false
+    
+    var profileButton: some View {
+        Button(action: { self.showingProfile.toggle() }) {
+            Image(systemName: "person.crop.circle")
+                .imageScale(.large)
+                .accessibility(label: Text("User Profile"))
+                .padding()
+        }
+    }
+
     var body: some View {
-        NavigationView{
+        NavigationView {
             List {
+                FeaturedLandmarks(landmarks: featured)
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
                 ForEach(categories.keys.sorted(), id: \.self) { key in
-                    Text(key)
+                    CategoryRow(categoryName: key, items: self.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+                
+                NavigationLink(destination: LandmarkList()) {
+                    Text("See All")
                 }
             }
             .navigationBarTitle(Text("Featured"))
+            .navigationBarItems(trailing: profileButton)
+            .sheet(isPresented: $showingProfile) {
+                Text("User Profile")
+            }
         }
     }
 }
 
-struct Home_Previews: PreviewProvider {
+struct FeaturedLandmarks: View {
+    var landmarks: [Landmark]
+    var body: some View {
+        landmarks[0].image.resizable()
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
     static var previews: some View {
         CategoryHome()
     }
--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -9,38 +9,33 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    
-    @EnvironmentObject var userData : UserData
-    
+    @EnvironmentObject var userData: UserData
+
     var body: some View {
-        NavigationView {
-            List {
-                
-                Toggle(isOn: $userData.showFavoritesOnly) {
-                    Text("Favorites Only")
-                        .font(.system(size: 25))
-            
-                }
-                .padding(5.0)
-                
-                
-                ForEach(userData.landmarks) { landmark in
-                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
-                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                            LandmarkRow(landmark: landmark)
-                        }
+
+        List {
+            Toggle(isOn: $userData.showFavoritesOnly) {
+                Text("Favorites only")
+            }
+
+            ForEach(userData.landmarks) { landmark in
+                if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
                     }
                 }
             }
-            .navigationBarTitle("Landmarks", displayMode: .automatic)
         }
-        .accentColor( .black)
+        .navigationBarTitle(Text("Landmarks"))
+
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
-        .environmentObject(UserData())
+        NavigationView {
+            LandmarkList()
+                .environmentObject(UserData())
+        }
     }
 }
